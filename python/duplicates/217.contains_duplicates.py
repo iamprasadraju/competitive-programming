2cca93efa6afd97d81588c4c376008ee245c2c13
iamprasadraju <workspace.iamprasadraju@gmail.com>
2025-10-11 18:16:41 +0530

    set - low memory usage

--- python/duplicates/217.contains_duplicates.py
+++ python/duplicates/217.contains_duplicates.py
@@ -1,15 +1,15 @@
 # leetcode Problem 217
 
-# using dict (hashmap)
+# using set
 
 def containsDuplicates(nums: List(int)) -> bool:
-	s = {}
+	s = set()
 	for num in nums:
 		if num in s:
 			return True
-		s[num] == 1
+		s.add(num)
 	return False
 
 
-# Runtime: 17ms | Beats 38.42%
-# Memory: 35.10 MB | Beats 10.01%
+# Runtime: 19ms | Beats 32.59%
+# Memory: 31.70 MB | Beats 29.59%
